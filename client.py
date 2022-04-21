454ebf15ad9c605e0e771a33384e6b1976224060
Guilherme Trindade Tolentino Bernardo <guilherme.bernardo@memt.com.br>
2022-04-21 17:05:49 -0300

    Version #1

--- client.py
+++ client.py
@@ -5,12 +5,12 @@
 import argparse
 import glob
 import gym
-import ipaddress
 import os
 import pickle
 import socket
 import sys
 import time
+import numpy as np
 from stable_baselines3 import A2C
 from stable_baselines3.common.utils import set_random_seed
 from utils import AGENTS
@@ -18,38 +18,23 @@ from utils import AGENTS
 
 class Client():
     def __init__(self, args):
-        self.ctrl = {'verbose': args['verbose'],
-                     'interval': args['interval'] * 1e6,
-                     'tolerance': args['tolerance'] * 1e6,
+        self.ctrl = {'verbose': args.verbose,
+                     'interval': args.interval * 1e6,
+                     'tolerance': args.tolerance * 1e6,
                      'supported_envs': [],
-                     'env_name': args['env'],
+                     'env_name': args.env,
                      'env': None,
-                     'agent_name': args['agent'],
-                     'agent': None}
+                     'agent_name': args.agent,
+                     'agent': None,
+                     'training': args.training,
+                     'timesteps': args.timesteps}
 
-        self.sock = {'ctrl': None, 'data': None}
-
-        self.sock = {'ctrl': socket.socket(socket.AF_INET, socket.SOCK_STREAM),
-                     'data': socket.socket(socket.AF_INET, socket.SOCK_DGRAM)}
-
-        try:
-            if isinstance(ipaddress.ip_address(args['address']), ipaddress.IPv4Address):
-                self.sock['ctrl'] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.sock['data'] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            elif isinstance(ipaddress.ip_address(args['address']), ipaddress.IPv6Address):
-                self.sock['ctrl'] = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-                self.sock['data'] = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-        except ValueError:
-            print('Error: invalid IP address {}'.format(args['address']))
-            sys.exit(1)
-
-        self.sock['ctrl'].connect((args['address'], args['control_port']))
-        self.sock['data'].connect((args['address'], args['port']))
-
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.connect((args.address, args.port))
         env_list, env_detail = self.get_supported_envs()
         self.ctrl['supported_envs'] = env_detail
 
-        if args['list_envs']:
+        if args.list_envs:
             print(env_list)
             sys.exit(0)
 
@@ -58,41 +43,82 @@ class Client():
             sys.exit(1)
 
         self.ctrl['env'] = gym.make(self.ctrl['env_name'])
-        self.ctrl['agent'] = self.load_trained_agent()
+
+        if self.ctrl['training'] == 'offline':
+            self.ctrl['agent'] = self.load_trained_agent()
+        elif self.ctrl['training'] == 'online':
+            print("Training...")
+            self.ctrl['agent'] = self.train_an_agent(self.ctrl['timesteps'])
+        else:
+            print('Training type not supported')
+            sys.exit(1)
 
         try:
             self.run()
         except KeyboardInterrupt:
-            result = self.ctrl_request({'finish': None})
-            print(result)
-            self.sock['ctrl'].close()
-            self.sock['data'].close()
+            self.sock.send(pickle.dumps({'finish': None}))
+            self.sock.close()
             sys.exit(0)
 
     def run(self):
-        reply = self.ctrl_request({'init': {'env_name': self.ctrl['env_name'],
-                                   'agent_name': self.ctrl['agent_name']}})
-
+        reply, _ = self.request({'init': {'env_name': self.ctrl['env_name'],
+                                          'agent_name': self.ctrl['agent_name']}})
         if reply.get('init', '') != 'OK':
             print('Failed to initialize remote environment')
             sys.exit(0)
-
-        reply = self.ctrl_request({'reset': None})
+        
+        tic = time.time() #counter init
+        episode_times = [] #vector to calculate the mean time and standard deviation of the first 100 episodes
+        episode_lenghts = [] #vector to calculate the mean reward and standard deviation of the first 100 episodes
+        timeouts_n = 0 #number of episodes that were not completed
+        ep_len = 0 #variable to count number of actions at each episodes
+        ep_num = 1 #episode number
+        reply, _ = self.request({'reset': None})
+        
         while True:
-            # Busy waiting, taking tolerance out of deadline
-            deadline = time.time_ns() + self.ctrl['interval']
-            while deadline - self.ctrl['tolerance'] > time.time_ns():
-                pass
+            try:
+                # Busy waiting, taking tolerance out of deadline
+                deadline = time.time_ns() + self.ctrl['interval']
+                while deadline - self.ctrl['tolerance'] > time.time_ns():
+                    pass
 
-            action, _state = self.ctrl['agent'].predict(reply['obs'], deterministic=False)
+                action, _state = self.ctrl['agent'].predict(reply['obs'], deterministic=False)
+                ep_len -= 1 #number of actions (reward)
 
-            # Send action and get reply
-            reply, _ = self.request({'action': action})
-            print(action, '/', reply)
+                # Send action and get reply
+                self.sock.settimeout(0.5) #prevents the client from waiting forever for a response from the server if any packets are lost
 
-            # Reset when done
-            if reply['done']:
-                reply = self.ctrl_request({'reset': None})
+                reply, _ = self.request({'action': action})
+
+                # Reset when done
+                if reply['done']:
+                    toc = time.time()
+                    elapsed_time = toc-tic
+                    episode_times.append(elapsed_time)
+                    episode_lenghts.append(ep_len)
+                    elapsed_time = "{:.6f}".format(toc-tic)
+                    print("------------------------")
+                    print("Episode: ", ep_num)
+                    ep_num += 1
+                    print("Episode Time (s): ", elapsed_time)
+                    print("Episode Reward: ", ep_len)
+                    ep_len  = 1
+                    print("Mean time: {:.2f} +/- {:.2f}".format(np.mean(episode_times), np.std(episode_times)))
+                    print("Mean lenght: {:.2f} +/- {:.2f}".format(np.mean(episode_lenghts), np.std(episode_lenghts)))
+                    tic = time.time()
+                    reply, _ = self.request({'reset': None})
+
+            except Exception: #if some packet is lost and there is a timeout in the request
+                if ep_num == 101: #last episode
+                    self.sock.close()
+                    sys.exit(0)
+                    break
+                ep_len -= 1
+                print("Timeout Exception")
+
+    def train(self, model, timesteps, callback=None, **kwargs):
+        model.learn(total_timesteps=timesteps, callback=callback, **kwargs)
+        return model
 
     def load_trained_agent(self):
         if self.ctrl['agent_name'] == 'auto':
@@ -109,18 +135,27 @@ class Client():
                                                                    self.ctrl['env_name'],
                                                                    self.ctrl['env_name'])
         agent_path = glob.glob(agent_env_path)[0]
-
         return AGENTS[self.ctrl['agent_name']].load(agent_path, env=self.ctrl['env'])
 
-    def request(self, msg):
-        self.sock['data'].send(pickle.dumps(msg))
-        reply, from_addr = self.sock['data'].recvfrom(1500)
-        return pickle.loads(reply), from_addr
+    def train_an_agent(self, timesteps):
+        if self.ctrl['agent_name'] == 'auto':
+            for key, _ in AGENTS.items():
+                if key.upper() in self.ctrl['supported_envs'].get(self.ctrl['env_name'], []):
+                    self.ctrl['agent_name'] = key
+                    break
 
-    def ctrl_request(self, msg):
-        self.sock['ctrl'].send(pickle.dumps(msg))
-        reply = self.sock['ctrl'].recv(1500)
-        return pickle.loads(reply)
+        if self.ctrl['agent_name'] not in AGENTS.keys():
+            print('Unable to find a trained agent for the {} environment'.format(self.ctrl['env_name']))
+            sys.exit(1)
+
+        model = AGENTS[self.ctrl['agent_name']]('MlpPolicy', self.ctrl['env'], verbose=0)
+        model = self.train(model, timesteps)
+        return model
+
+    def request(self, msg):
+        self.sock.send(pickle.dumps(msg))
+        reply, from_addr = self.sock.recvfrom(1500)
+        return pickle.loads(reply), from_addr
 
     def get_supported_envs(self):
         gym_envs = [e.id for e in gym.envs.registry.all()]
@@ -137,8 +172,7 @@ class Client():
         del supported_envs['best_model']
 
         # Get what exists on server node
-        reply = self.ctrl_request({'get_supported_envs': None})
-        print(reply)
+        reply, _ = self.request({'get_supported_envs': None})
         intersection = set(reply['supported_envs']).intersection(set(supported_envs.keys()))
 
         return sorted(list(intersection)), supported_envs
@@ -146,18 +180,19 @@ class Client():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RL Client Simple')
-    parser.add_argument('-a', '--address', type=str, default='192.168.122.74', help='Server address')
+    parser.add_argument('-a', '--address', type=str, default='192.168.1.15', help='Server address')
     parser.add_argument('-p', '--port', type=int, default=9000, help='Server port')
-    parser.add_argument('-c', '--control-port', type=int, default=9001, help='Control port')
-    parser.add_argument('-i', '--interval', type=int, default=10, help='Max interval between actions')
+    parser.add_argument('-i', '--interval', type=int, default=10, help='Max interval between actions (ms)')
     parser.add_argument('-t', '--tolerance', type=int, default=2, help='Tolerance on interval')
-    parser.add_argument('-e', '--env', type=str, default='CartPole-v1',
+    parser.add_argument('-e', '--env', type=str, default='Acrobot-v1',
                         help='OpenAI Gym Environment. Use -l to see all environments available')
     parser.add_argument('-g', '--agent', type=str, default='auto',
                         help='Agent in [a2c ddpg dqn her ppo qrdqn sac td3 tqc]')
     parser.add_argument('-l', '--list-envs', action='store_true',
                         help='List supported environments (server must be running)')
     parser.add_argument('-v', '--verbose', type=int, default=0, help='Verbosity level')
-    args = vars(parser.parse_args())
+    parser.add_argument('-r', '--training', type=str, default='offline', help='Choose between online and offline training')
+    parser.add_argument('-s', '--timesteps', type=int, default=20000, help='Number of timesteps to train online an agent')
+    args = parser.parse_args()
 
     Client(args)
--- server.py
+++ server.py
@@ -5,69 +5,46 @@
 import argparse
 import glob
 import gym
-import ipaddress
 import os
 import pickle
-import select
 import socket
 import sys
 import time
+import numpy as np
 
-from stable_baselines3 import A2C
+#from stable_baselines3 import A2C
 from utils import AGENTS
 
 
 class Server():
     def __init__(self, args):
-        self.ctrl = {'interval': args['interval'] * 1e6,
-                     'tolerance': args['tolerance'] * 1e6,
+        self.ctrl = {'interval': args.interval * 1e6,
+                     'tolerance': args.tolerance * 1e6,
                      'env_name': None,
                      'env': None,
                      'agent_name': None,
-                     'local_agent': None,
-                     'verbose': args['verbose']}
+                     'agent': None,
+                     'verbose': args.verbose,
+                     'training': args.training,
+                     'timesteps': args.timesteps}
 
         self.state = {'obs': None,
                       'reward': None,
                       'done': False,
                       'info': None}
 
-        self.sock = {'ctrl': None, 'data': None, 'ctrl_conn': None}
-
-        try:
-            if isinstance(ipaddress.ip_address(args['address']), ipaddress.IPv4Address):
-                self.sock['ctrl'] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.sock['data'] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            elif isinstance(ipaddress.ip_address(args['address']), ipaddress.IPv6Address):
-                self.sock['ctrl'] = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-                self.sock['data'] = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-        except ValueError:
-            print('Error: invalid IP address {}'.format(args['address']))
-            sys.exit(1)
-
-        self.sock['ctrl'].bind((args['address'], args['control_port']))
-        self.sock['ctrl'].listen()
-        self.sock['data'].bind((args['address'], args['port']))
-        self.sock['data'].setblocking(False)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((args.address, args.port))
 
         try:
             self.run()
         except KeyboardInterrupt:
-            self.sock['ctrl'].close()
-            self.sock['data'].close()
             sys.exit(0)
 
     def run(self):
-        conn, from_addr = self.sock['ctrl'].accept()
-        self.sock['ctrl_conn'] = conn
-
         while True:
-            request = self.ctrl_receive()
-
-            if 'get_supported_envs' in request:
-                supported_envs = self.get_supported_envs()
-                self.ctrl_reply({'supported_envs': supported_envs})
-            elif 'init' in request:
+            request, from_addr = self.receive()
+            if 'init' in request:
                 # Get and initialize environment
                 self.ctrl['env_name'] = request.get('init', {}).get('env_name')
                 self.ctrl['env'] = gym.make(self.ctrl['env_name'])
@@ -75,80 +52,95 @@ class Server():
 
                 # Get and initialize local agent
                 self.ctrl['agent_name'] = request.get('init', {}).get('agent_name')
-                self.ctrl['local_agent'] = self.load_local_agent()
+                if self.ctrl['training'] == 'offline':
+                    self.ctrl['agent'] = self.load_local_agent()
+                elif self.ctrl['training'] == 'online':
+                    self.ctrl['agent'] = self.train_an_agent(self.ctrl['timesteps'])
+                else:
+                    print('Training type not supported')
+                    sys.exit(1)
 
                 # Reply to client and proceed to control loop
-                self.ctrl_reply({'init': 'OK'})
-                self.sock['ctrl_conn'].setblocking(False)
+                self.reply({'init': 'OK'}, from_addr)
                 break
+            elif 'get_supported_envs' in request:
+                supported_envs = self.get_supported_envs()
+                self.reply({'supported_envs': supported_envs}, from_addr)
             else:
-                print('Unknown request:', request)
+                print('Initialization failed')
+                sys.exit(0)
+            time.sleep(0.01)
+
+        # After initialization, disable blocking
+        self.sock.setblocking(False)
+        
+        episode_lengths = []
+        ep_len = 0
+        finish_game = 0
+        first_time = True #begins with reset and reward = 0
 
         deadline = time.time_ns() + self.ctrl['interval']
-        stats = {'episode_reward': 0,
-                 'episode_steps': 0,
-                 'deadlines_missed': 0,
-                 'episode': 0}
-        done_on_deadline = False
+        self.sock.settimeout(2)
         while True:
-            # First check control messages
-            try:
-                request = self.ctrl_receive()
-                print('CTRL request:', request)
-                if 'reset' in request:
-                    self.state['obs'] = self.ctrl['env'].reset()
-                    reply = {'obs': self.state['obs'],
-                             'stats': stats}
-                    self.ctrl_reply(reply)
-                    stats['episode_reward'] = 0
-                    stats['episode_steps'] = 0
-                    stats['deadlines_missed'] = 0
-                    stats['episode'] += 1
-                elif 'finish' in request:
-                    self.ctrl_reply({'finish': 'OK', 'stats': stats})
-                    self.sock['ctrl'].close()
-                    self.sock['data'].close()
-                    sys.exit(0)
-            except Exception:
-                pass
-
-            # Now check data messages
             try:
                 request, from_addr = self.receive()
 
-                if done_on_deadline:
-                    self.state['obs'], self.state['reward'], self.state['done'], self.state['info'] = end_data
-                    done_on_deadline = False
-                else:
-                    if 'action' in request:
-                        self.state['obs'], self.state['reward'], self.state['done'], self.state['info'] = self.ctrl['env'].step(request['action'])  # noqa
-                        reply = self.state
-                        deadline += self.ctrl['interval']
-                        stats['episode_reward'] += self.state['reward']
-                        stats['episode_steps'] += 1
+                if finish_game == 100: #after 100 episode executions
+                    if len(episode_lengths) > 0:
+                        print("Mean length: {:.2f} +/- {:.2f}".format(np.mean(episode_lengths), np.std(episode_lengths)))
+                    self.sock.close()
+                    sys.exit(0)
+                    break
 
-                print(reply)
+                if 'reset' in request:
+                    self.state['obs'] = self.ctrl['env'].reset()
+                    if not first_time:
+                        episode_lengths.append(ep_len) #after 1 episode execution, store the rewards
+                        print("Episode Lenght:", ep_len)
+                        finish_game += 1 #variable to stop the game execution after 100 rewards
+                        ep_len = 0
+                    first_time = False
+                    #time.sleep(1)
+                    reply = {'obs': self.state['obs']}
+                elif 'action' in request:
+                    self.state['obs'], self.state['reward'], self.state['done'], self.state['info'] = self.ctrl['env'].step(request['action']) # noqa
+                    ep_len += self.state['reward'] #the reward is based on the number of actions performed. the greater the number of actions, the lower the reward (10 actions = -10 reward points)
+                    reply = self.state
+                elif 'finish' in request:
+                    self.sock.close()
+                    sys.exit(0)
+                    break
+
                 self.reply(reply, from_addr)
+                deadline = time.time_ns() + self.ctrl['interval']
 
             except Exception:
-                if time.time_ns() > deadline:
+                if time.time_ns() < deadline:
+                    pass
+                else:
                     deadline = deadline + self.ctrl['interval']
-                    action, _ = self.ctrl['local_agent'].predict(self.state['obs'], deterministic=False)
+                    # Take random action provided by our local untrained model
+                    action, _ = self.ctrl['agent'].predict(self.state['obs'], deterministic=False)
                     self.state['obs'], self.state['reward'], self.state['done'], self.state['info'] = self.ctrl['env'].step(action)  # noqa
+                    ep_len += self.state['reward'] 
                     print('.', end='')
                     sys.stdout.flush()
-                    stats['episode_reward'] += self.state['reward']
-                    stats['episode_steps'] += 1
-                    stats['deadlines_missed'] += 1
-
-                    if self.state['done']:
-                        end_data = self.ctrl['env'].reset()
-                        done_on_deadline = True
 
             self.ctrl['env'].render(mode='human')
 
+    def train(self, model, timesteps, callback=None, **kwargs):
+        model.learn(total_timesteps=timesteps, callback=callback, **kwargs)
+        return model
+
     def load_local_agent(self):
-        return AGENTS[self.ctrl['agent_name']]('MlpPolicy', self.ctrl['env'], verbose=0)
+        m = AGENTS[self.ctrl['agent_name']]('MlpPolicy', self.ctrl['env'], verbose=0)
+        return m
+
+    def train_an_agent(self, timesteps):
+        m = AGENTS[self.ctrl['agent_name']]('MlpPolicy', self.ctrl['env'], verbose=0)
+        print('Training...')
+        m = self.train(m, timesteps)
+        return m
 
     def get_supported_envs(self):
         gym_envs = [e.id for e in gym.envs.registry.all()]
@@ -166,28 +158,22 @@ class Server():
         return sorted(supported_envs.keys())
 
     def receive(self):
-        request, from_addr = self.sock['data'].recvfrom(1500)
+        request, from_addr = self.sock.recvfrom(1500)
         return pickle.loads(request), from_addr
 
-    def ctrl_receive(self):
-        request = self.sock['ctrl_conn'].recv(1500)
-        return pickle.loads(request)
-
     def reply(self, msg, addr):
-        self.sock['data'].sendto(pickle.dumps(msg), addr)
-
-    def ctrl_reply(self, msg):
-        self.sock['ctrl_conn'].send(pickle.dumps(msg))
+        self.sock.sendto(pickle.dumps(msg), addr)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RL Client Simple')
     parser.add_argument('-a', '--address', type=str, default='0.0.0.0', help='Server address')
     parser.add_argument('-p', '--port', type=int, default=9000, help='Server port')
-    parser.add_argument('-c', '--control-port', type=int, default=9001, help='Control port')
     parser.add_argument('-i', '--interval', type=int, default=10, help='Max interval between actions (ms)')
     parser.add_argument('-t', '--tolerance', type=int, default=2, help='Tolerance on interval')
     parser.add_argument('-v', '--verbose', type=int, default=0, help='Verbosity level')
-    args = vars(parser.parse_args())
+    parser.add_argument('-r', '--training', type=str, default='offline', help='Choose between online and offline training')
+    parser.add_argument('-s', '--timesteps', type=int, default=20000, help='Number of timesteps to traing an agent')
+    args = parser.parse_args()
 
     Server(args)
